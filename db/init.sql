36a4864bfbfe0d4c88ae90e6c9a3b6f6a07644e8
RosaMoss <85589945+RosaMoss@users.noreply.github.com>
2021-08-02 06:03:30 -0400

    added user login

---
+++ db/init.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE citiesData;
+use citiesData;
+
+CREATE TABLE IF NOT EXISTS addresses (
+    `Address_id` INT,
+    `First_Name` VARCHAR(21) CHARACTER SET utf8,
+    `Last_Name` VARCHAR(8) CHARACTER SET utf8,
+    `Street` VARCHAR(32) CHARACTER SET utf8,
+    `City` VARCHAR(11) CHARACTER SET utf8,
+    `State` VARCHAR(3) CHARACTER SET utf8,
+    `Postal_Code` INT
+);
+INSERT INTO addresses VALUES
+    (0,'John','Doe','120 jefferson st.','Riverside',' NJ', 08075),
+    (1,'Jack','McGinnis','220 hobo Av.','Phila',' PA',09119),
+    (2,'John "Da Man"','Repici','120 Jefferson St.','Riverside',' NJ',08075),
+    (3,'Stephen','Tyler','7452 Terrace "At the Plaza" road','SomeTown','SD', 91234),
+    (4,NULL,'Blankman',NULL,'SomeTown',' SD', 00298),
+    (5,'Joan "the bone", Anne','Jet','9th, at Terrace plc','Desert City','CO',00123);
+    
+CREATE TABLE IF NOT EXISTS users (
+    `id` int AUTO_INCREMENT,
+    `email` varchar(100),
+    `password` varchar(550),
+    PRIMARY KEY (`id`)
+);
+INSERT INTO users (email, password) VALUES
+    ('s.sescalante@gmail.com','password'),
+    ('t.thompson@gmail.com', 'password2');
